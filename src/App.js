30869ff25e5fd97552310133abcb0d1820636186
ieatn <ieatnubcake@gmail.com>
2022-11-28 15:19:20 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useEffect, useState} from 'react';
+
+const API_URL = 'https://api.chucknorris.io/jokes/random'
 
 function App() {
+
+  const [joke, setJoke] = useState('')
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setJoke(data.value))
+  }
+
+  useEffect(() => {
+    generateJoke();
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'> 
+      <h2>Chuck Norris Joke Generator</h2>
+      <p>{joke}</p>
+      <button onClick={generateJoke}>Get new joke 😂</button>
     </div>
   );
 }
